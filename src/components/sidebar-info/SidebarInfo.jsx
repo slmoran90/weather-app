1cf45b0fe54a2c0a5fbc0863e0f490a137c551f3
Sergio Moran <moransergio.01@gmail.com>
2022-08-24 02:47:37 -0300

    add data in SidebarInfo

--- src/components/sidebar-info/SidebarInfo.jsx
+++ src/components/sidebar-info/SidebarInfo.jsx
@@ -1,29 +1,45 @@
 import SidebarHeader from './header/SidebarHeader'
 import imgWeather from '../../assets/img/LightCloud.png'
 import './sidebarInfo.scss'
+import dayjs from 'dayjs'
 
-const SidebarInfo = () => {
+const SidebarInfo = ({ data, setData, setLocation }) => {
+  const { name, temp_c, condition, localtime, region } = data
+
+  const getDate = date => {
+    const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+    const week = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab']
+
+    const localdate = date?.slice(0, 10)
+    const d = dayjs(localdate)
+
+    return `${week[d.$W]}, ${d.$D} de ${months[d.$M]}`
+  }
+
+  const date = getDate(localtime)
+
   return (
     <div className='main-container container'>
-      <SidebarHeader/>
+      <SidebarHeader setLocation={setLocation}/>
       <div className='main-container__body row'>
         <div className='col-sm-12'>
           <img src={imgWeather} alt='weather image' className='img-fluid' loading='lazy'/>
         </div>
         <div className='col-sm-12 temp-container'>
           <p className='temp'>
-            15
+            {temp_c}
             <span>°c</span>
           </p>
-          <p className='condition'>shower</p>
+          <p className='condition'>{condition?.text}</p>
           <p className='date'>
             <span>Hoy</span>
             <span>•</span>
-            <span>Vier, 2 de Julio</span>
+            <span>{date}</span>
           </p>
           <p className='location'>
             <span className='material-icons'>location_on</span>
-            Tucumán
+            {name} - {region}
           </p>
         </div>
       </div>
