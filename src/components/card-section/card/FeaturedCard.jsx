35177bfa613a3dcd2ce33d44d0d660d3f9f9c14a
Sergio Morán <moransergio.01@gmail.com>
2022-08-25 01:46:31 -0300

    add integration cards (#7)

--- src/components/card-section/card/FeaturedCard.jsx
+++ src/components/card-section/card/FeaturedCard.jsx
@@ -1,41 +1,26 @@
 import './card.scss'
 
-const FeaturedCard = ({ cardStatus }) => {
-  let status = {
-    key: null,
-    title: null,
-    value: null
-  }
-
-  Object.entries(cardStatus).map(([key, values]) => {
-    const { title, value } = values
-    status = {
-      key,
-      title,
-      value
-    }
-    return status
-  })
-
+const FeaturedCard = ({ data }) => {
+  const { title, label, value } = data
   return (
     <div
       className='card-container card'
       style={{ width: '328px', height: '205px' }}
     >
       <div className='card-body d-flex flex-column align-items-center justify-content-between'>
-        <h5 className='title text-capitalize'>{status.title}</h5>
+        <h5 className='title text-capitalize'>{title}</h5>
         <div className='card-text w-100 justify-content-center flex-column align-items-center'>
             <p className='status-value'>
-              {status.value}
+              {value}
               <span>
                 {
-                  status.key === 'windStatus' ? ' km/h' : status.key === 'humidity' ? '%' : status.key === 'visibility' ? ' km' : ' mb'
+                  label === 'windStatus' ? ' km/h' : label === 'humidity' ? '%' : label === 'visibility' ? ' km' : ' mb'
                 }
               </span>
             </p>
             {
-              status.key === 'humidity'
-                ? <div className='range' style={{ '--range-width': status.value }}>
+              label === 'humidity'
+                ? <div className='range' style={{ '--range-width': value }}>
                   <div className='range-label'>
                     <p>0</p>
                     <p>50</p>
--- src/components/card-section/card/ForecastCard.jsx
+++ src/components/card-section/card/ForecastCard.jsx
@@ -1,19 +1,28 @@
-import imgCard from '../../../assets/img/LightRain.png'
+// import imgCard from '../../../assets/img/LightRain.png'
+import { getDate } from '../../../helper/getDate'
 import './card.scss'
 
-const ForecastCard = ({ card }) => {
-  const { day, maxTemp, minTemp } = card
+const ForecastCard = ({ forecastData, localtime }) => {
+  const { day, date } = forecastData
+  const { maxtemp_c, mintemp_c, condition } = day
+
+  const d = getDate(date)
+  const dateNow = getDate(localtime)
+
+  const getWeek = () => d.day === dateNow.day ? 'Hoy' : d.day === (dateNow.day + 1) ? 'Mañana' : d.week
+
+  const week = getWeek()
 
   return (
     <div className='card-container card' style={{ width: '120px', height: '180px' }}>
       <div className='card-body d-flex flex-column align-items-center justify-content-between'>
-        <h5 className='title'>{day}</h5>
+        <h5 className='title'>{week}</h5>
         <div className='img-container'>
-          <img src={imgCard} alt='image temp' className='img-fluid' loading='lazy'/>
+          <img src={condition?.icon} alt='image temp' className='img-fluid' loading='lazy'/>
         </div>
         <div className="card-text w-100 justify-content-around temp-min-max">
-          <p>{maxTemp}°C</p>
-          <p>{minTemp}°C</p>
+          <p>{mintemp_c}°C</p>
+          <p>{maxtemp_c}°C</p>
         </div>
       </div>
     </div>
