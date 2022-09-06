e21bdb9aca985ba705a4462a7cb224a0ee827eeb
Sergio Morán <moransergio.01@gmail.com>
2022-09-06 20:22:30 -0300

    add spinner and some fixes (#21)

--- src/components/card-section/CardSection.jsx
+++ src/components/card-section/CardSection.jsx
@@ -8,8 +8,10 @@ import 'aos/dist/aos.css'
 import './cardSection.scss'
 
 const CardSection = () => {
-  const { forecast } = useContext(ForecastContext)
+  const { forecast, loading } = useContext(ForecastContext)
   const { forecastday, localtime, wind_kph, humidity, vis_km, pressure_mb } = forecast
+
+  console.log('loading', loading)
 
   const hl = [
     {
@@ -34,32 +36,41 @@ const CardSection = () => {
     }
   ]
 
+  const load =
+    <div className="spinner-grow spinner-grow-sm text-light d-none d-md-block" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+
   AOS.init()
 
   return (
     <div className='pb-2 min-vh-100 cs-container col-12 col-md-8 row gx-0'>
       <div className='gap-5 py-5 row gx-0 d-flex justify-content-start justify-content-md-center'>
         {
-          forecastday?.map((forecastData, i) => (
-            <div
-              className={`col-5 col-md-1 d-flex justify-content-center`}
-              key={i}
-              data-aos={i % 2 === 0 ? 'zoom-out-right' : 'zoom-out-left'}
-              data-aos-duration="450"
-              data-aos-delay={i % 2 === 0 ? '50' : '150'}
-            >
-              <ForecastCard forecastData={forecastData} localtime={localtime}/>
-            </div>
-          ))
+          loading
+            ? (load)
+            : forecastday?.map((forecastData, i) => {
+              return (
+              <div
+                className={`col-5 col-md-1 d-flex justify-content-center`}
+                key={i}
+                data-aos={i % 2 === 0 ? 'zoom-out-right' : 'zoom-out-left'}
+                data-aos-duration="450"
+                data-aos-delay={i % 2 === 0 ? '50' : '150'}
+              >
+                <ForecastCard forecastData={forecastData} localtime={localtime}/>
+              </div>
+              )
+            })
         }
       </div>
       <div className='mt-5 highlights'>
         <h5 className='py-3 w-100 w-md-25 highlights__title'>Destacados de hoy</h5>
-        <div className="gap-5 row gx-0 w-100">
+        <div className="gap-3 gap-md-5 row gx-0 w-100 pb-4">
           {
             hl.map((data, i) => (
               <div
-                className="col-md-5 col-12 d-flex justify-content-center"
+                className="col-md-5 col-12 d-flex justify-content-center justify-content-md-end"
                 key={i}
                 data-aos="zoom-in-up"
                 data-aos-anchor-placement="top-bottom"
--- src/context/forecastContext.jsx
+++ src/context/forecastContext.jsx
@@ -6,10 +6,13 @@ export const ForecastContext = createContext()
 const ForecastProvider = ({ children }) => {
   const [forecast, setForecast] = useState({})
   const [location, setLocation] = useState('argentina')
+  const [loading, setLoading] = useState(false)
 
   const getdata = async () => {
     try {
+      setLoading(true)
       const data = await fetchData(location)
+      setLoading(false)
       return setForecast(data)
     } catch (error) {
       console.log(error)
@@ -21,7 +24,7 @@ const ForecastProvider = ({ children }) => {
   }, [location])
 
   return (
-    <ForecastContext.Provider value={{ forecast, setForecast, location, setLocation }}>
+    <ForecastContext.Provider value={{ forecast, setForecast, location, setLocation, loading }}>
       { children }
     </ForecastContext.Provider>
   )
